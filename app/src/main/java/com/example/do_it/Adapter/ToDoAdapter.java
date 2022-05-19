0d0e7ba08669dd78627cfb54718d2f97f9482d81
Sarika Kushwaha <sarika.vsa01@gmail.com>
2022-05-19 09:59:33 +0530

    added new directory in java.com.example.do_it package

--- app/src/main/java/com/example/do_it/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/do_it/Adapter/ToDoAdapter.java
@@ -4,9 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.do_it.MainActivity;
 import com.example.do_it.Model.ToDoModel;
 import com.example.do_it.R;
