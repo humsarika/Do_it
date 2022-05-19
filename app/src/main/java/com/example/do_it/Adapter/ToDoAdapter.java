1387d359c829e88c7c7c744d22754149d33d36a8
Sarika Kushwaha <sarika.vsa01@gmail.com>
2022-05-19 09:53:15 +0530

    added new package in java.com.example.do_it

---
+++ app/src/main/java/com/example/do_it/Adapter/ToDoAdapter.java
@@ -0,0 +1,51 @@
+package com.example.do_it.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.do_it.MainActivity;
+import com.example.do_it.Model.ToDoModel;
+import com.example.do_it.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+    private List<ToDoModel> toDoList;
+    private MainActivity activity;
+
+    public ToDoAdapter (MainActivity activity ){
+        this.activity = activity;
+    }
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout,parent,false);
+        return new ViewHolder(itemView);
+    }
+    public void onBindViewHolder(ViewHolder holder , int position){
+        ToDoModel item = toDoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+    public int getItemCount(){
+        return toDoList.size();
+    }
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+    public void setTask(List<ToDoModel>toDoList){
+        this.toDoList = toDoList;
+        notifyDataSetChanged();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById((R.id.checkbox));
+        }
+    }
+}
