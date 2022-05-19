c593bb3036b9ac6ec9dd48b177f8f7b4bd699a04
Sarika Kushwaha <sarika.vsa01@gmail.com>
2022-05-19 09:34:31 +0530

    updated code for splash activity

--- app/src/main/java/com/example/do_it/splashActivity.java
+++ app/src/main/java/com/example/do_it/splashActivity.java
@@ -2,7 +2,9 @@ package com.example.do_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splashActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class splashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
 
+        final Intent i = new Intent(splashActivity.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
     }
 }
